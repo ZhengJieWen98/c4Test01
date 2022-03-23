ea87250a80b3392b2eabc7b9e81fc3b8b80ba426
ZhengJieWen <38306482@qq.com>
2022-03-24 00:33:27 +0800

    把所有数据读取出来

---
+++ myZhengJieWen/src/main/java/csdn/pojo/Department.java
@@ -0,0 +1,307 @@
+package csdn.pojo;
+
+/**
+ * 部门
+ */
+public class Department {
+    private String department;//部门
+    private String jobNumber;//工号
+    private String name;//姓名
+    private double basicSalary;//底薪
+    private double postWage;//岗位工资
+    private double performanceBonus;//绩效奖金
+    private double attendanceBonus;//全勤奖金
+    private double attendanceDeduction;//考勤扣除
+    private double penalties_for_non_compliance;//违规处罚
+    private double transportationSubsidies;//交通补助
+    private double communicationsGrants;//通信补助
+
+    private double endowmentInsuranceSelf;//个人养老保险
+    private double endowmentInsuranceCompany;//公司养老保险
+
+    private double medicareSelf;//个人医疗保险
+    private double medicareCompany;//公司医疗保险
+
+    private double unemploymentInsuranceSelf;//个人失业保险
+    private double unemploymentInsuranceConpany;//公司失业保险
+
+    private double employmentInjuryInsuranceSelf;//个人工伤保险
+    private double employmentInjuryInsuranceConpany;//公司工伤保险
+
+    private double birthInsuranceSelf;//个人生育保险
+    private double birthInsuranceConpany;//公司生育保险
+
+    private double reservedFundsSelf;//个人公积金
+    private double reservedFundsConpany;//公司公积金
+
+    @Override
+    public String toString() {
+        return "工号:"+jobNumber+
+                " 姓名:"+name+
+                " 部门:"+department+
+                " 工资:"+getAdd()+
+                " 扣款:"+getMinusYield()+
+                "养老(个人):"+endowmentInsuranceSelf+
+                "医疗(个人):"+medicareSelf+
+                "失业(个人):"+unemploymentInsuranceSelf+
+                "工伤(个人):"+employmentInjuryInsuranceSelf+
+                "生育(个人):"+birthInsuranceSelf+
+                "公积金(个人):"+reservedFundsSelf+
+                "合计(个人):"+getSelf()+
+                "养老(公司):"+endowmentInsuranceCompany+
+                "医疗(公司):"+medicareCompany+
+                "失业(公司):"+unemploymentInsuranceConpany+
+                "工伤(公司):"+employmentInjuryInsuranceConpany+
+                "生育(公司):"+birthInsuranceConpany+
+                "公积金(公司):"+reservedFundsConpany+
+                "合计(公司):"+getConpany()+
+                "个税金额:"+getTax()+
+                " 应发工资:"+getSalary()+
+                "实发工资"+getNetPayroll()+
+                "企业支出成本:"+getAllConpany();
+
+    }
+
+    //个人缴纳部分
+    public double getSelf(){
+        return endowmentInsuranceSelf+medicareSelf+unemploymentInsuranceSelf+employmentInjuryInsuranceSelf+birthInsuranceSelf+reservedFundsSelf;
+    }
+    //企业缴纳部分
+    public double getConpany(){
+        return endowmentInsuranceCompany+medicareCompany+unemploymentInsuranceConpany+employmentInjuryInsuranceConpany+birthInsuranceConpany+reservedFundsConpany;
+    }
+    //应发工资+企业缴纳五险一金”的结果填入“企业人员月度工资成本支付表.xlsx“的“企业支出成本”一栏。
+    public double getAllConpany(){
+        return getSalary()+getConpany();
+    }
+    //应发工资-五险一金个人缴纳部分”的结果等于“应税金额” 个人税
+    public double getTax(){
+        double gz = getSalary()-getSelf();
+        double s;
+        if(gz<3000){
+            s=gz*0.003;
+        }else if(gz<=12000){
+            s=gz*0.1;
+        }else if(gz<=25000){
+            s=gz*0.2;
+        }else if(gz<=35000){
+            s=gz*0.25;
+        }else if(gz<=55000){
+            s=gz*0.3;
+        }else if(gz<=80000){
+            s=gz*0.35;
+        }else {
+            s=gz*0.45;
+        }
+        return s;
+    }
+
+    //“应发工资-五险一金个人缴纳部分-个税”的结果填入“企业人员月度工资成本支付表.xlsx“的“实发工资”一栏。
+    public double getNetPayroll(){
+        return getSalary()-getSelf()-getTax();
+    }
+
+    public double getEndowmentInsuranceSelf() {
+        return endowmentInsuranceSelf;
+    }
+
+    public void setEndowmentInsuranceSelf(double endowmentInsuranceSelf) {
+        this.endowmentInsuranceSelf = endowmentInsuranceSelf;
+    }
+
+    public double getEndowmentInsuranceCompany() {
+        return endowmentInsuranceCompany;
+    }
+
+    public void setEndowmentInsuranceCompany(double endowmentInsuranceCompany) {
+        this.endowmentInsuranceCompany = endowmentInsuranceCompany;
+    }
+
+    public double getMedicareSelf() {
+        return medicareSelf;
+    }
+
+    public void setMedicareSelf(double medicareSelf) {
+        this.medicareSelf = medicareSelf;
+    }
+
+    public double getMedicareCompany() {
+        return medicareCompany;
+    }
+
+    public void setMedicareCompany(double medicareCompany) {
+        this.medicareCompany = medicareCompany;
+    }
+
+    public double getUnemploymentInsuranceSelf() {
+        return unemploymentInsuranceSelf;
+    }
+
+    public void setUnemploymentInsuranceSelf(double unemploymentInsuranceSelf) {
+        this.unemploymentInsuranceSelf = unemploymentInsuranceSelf;
+    }
+
+    public double getUnemploymentInsuranceConpany() {
+        return unemploymentInsuranceConpany;
+    }
+
+    public void setUnemploymentInsuranceConpany(double unemploymentInsuranceConpany) {
+        this.unemploymentInsuranceConpany = unemploymentInsuranceConpany;
+    }
+
+    public double getEmploymentInjuryInsuranceSelf() {
+        return employmentInjuryInsuranceSelf;
+    }
+
+    public void setEmploymentInjuryInsuranceSelf(double employmentInjuryInsuranceSelf) {
+        this.employmentInjuryInsuranceSelf = employmentInjuryInsuranceSelf;
+    }
+
+    public double getEmploymentInjuryInsuranceConpany() {
+        return employmentInjuryInsuranceConpany;
+    }
+
+    public void setEmploymentInjuryInsuranceConpany(double employmentInjuryInsuranceConpany) {
+        this.employmentInjuryInsuranceConpany = employmentInjuryInsuranceConpany;
+    }
+
+    public double getBirthInsuranceSelf() {
+        return birthInsuranceSelf;
+    }
+
+    public void setBirthInsuranceSelf(double birthInsuranceSelf) {
+        this.birthInsuranceSelf = birthInsuranceSelf;
+    }
+
+    public double getBirthInsuranceConpany() {
+        return birthInsuranceConpany;
+    }
+
+    public void setBirthInsuranceConpany(double birthInsuranceConpany) {
+        this.birthInsuranceConpany = birthInsuranceConpany;
+    }
+
+    public double getReservedFundsSelf() {
+        return reservedFundsSelf;
+    }
+
+    public void setReservedFundsSelf(double reservedFundsSelf) {
+        this.reservedFundsSelf = reservedFundsSelf;
+    }
+
+    public double getReservedFundsConpany() {
+        return reservedFundsConpany;
+    }
+
+    public void setReservedFundsConpany(double reservedFundsConpany) {
+        this.reservedFundsConpany = reservedFundsConpany;
+    }
+
+    public Department(){
+
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getJobNumber() {
+        return jobNumber;
+    }
+
+    public void setJobNumber(String jobNumber) {
+        this.jobNumber = jobNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBasicSalary() {
+        return basicSalary;
+    }
+
+    public void setBasicSalary(double basicSalary) {
+        this.basicSalary = basicSalary;
+    }
+
+    public double getPostWage() {
+        return postWage;
+    }
+
+    public void setPostWage(double postWage) {
+        this.postWage = postWage;
+    }
+
+    public double getPerformanceBonus() {
+        return performanceBonus;
+    }
+
+    public void setPerformanceBonus(double performanceBonus) {
+        this.performanceBonus = performanceBonus;
+    }
+
+    public double getAttendanceBonus() {
+        return attendanceBonus;
+    }
+
+    public void setAttendanceBonus(double attendanceBonus) {
+        this.attendanceBonus = attendanceBonus;
+    }
+
+    public double getAttendanceDeduction() {
+        return attendanceDeduction;
+    }
+
+    public void setAttendanceDeduction(double attendanceDeduction) {
+        this.attendanceDeduction = attendanceDeduction;
+    }
+
+    public double getPenalties_for_non_compliance() {
+        return penalties_for_non_compliance;
+    }
+
+    public void setPenalties_for_non_compliance(double penalties_for_non_compliance) {
+        this.penalties_for_non_compliance = penalties_for_non_compliance;
+    }
+
+    public double getTransportationSubsidies() {
+        return transportationSubsidies;
+    }
+
+    public void setTransportationSubsidies(double transportationSubsidies) {
+        this.transportationSubsidies = transportationSubsidies;
+    }
+
+    public double getCommunicationsGrants() {
+        return communicationsGrants;
+    }
+
+    public void setCommunicationsGrants(double communicationsGrants) {
+        this.communicationsGrants = communicationsGrants;
+    }
+
+    //得到正收益和
+    public double getAdd(){
+        return basicSalary+postWage+performanceBonus+attendanceBonus+transportationSubsidies+communicationsGrants;
+    }
+    ////考勤扣除、违规处罚等负收益项
+    public double getMinusYield(){
+        return attendanceDeduction+penalties_for_non_compliance;
+    }
+
+    //应发工资=工资-扣款
+    public double getSalary(){
+        return getAdd()-getMinusYield();
+    }
+
+
+}
--- myZhengJieWen/src/main/java/csdn/util/ExcelUtil.java
+++ myZhengJieWen/src/main/java/csdn/util/ExcelUtil.java
@@ -1,49 +1,167 @@
 package csdn.util;
 
+import csdn.pojo.Department;
 import csdn.pojo.Person;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
 
 public class ExcelUtil {
     public static void main(String[] args) throws IOException {
-        List<Person> list = new ArrayList<Person>();
-        list.add(new Person("张三","成都","12345678910"));
-        list.add(new Person("李四","北京","6666666666"));
-        list.add(new Person("王麻子","上海","8888888888"));
-        ExcelUtil excelWriter = new ExcelUtil();
-        excelWriter.fillExcel(list,new File("C:\\Users\\38306\\Desktop\\persion.xlsx"));
+        Map<String,Department> map = new TreeMap<String, Department>();
+        ExcelUtil excelUtil = new ExcelUtil();
+        //获取到部门员工的数据
+        InputStream resourceAsStream1 = excelUtil.getClass().getClassLoader().getResourceAsStream("研发部-薪酬表.xlsx");
+        excelUtil.readExcel_Department("研发部", resourceAsStream1,map);
+        //获取到部门员工的数据
+        InputStream resourceAsStream2 = excelUtil.getClass().getClassLoader().getResourceAsStream("大客户部-薪酬表.xlsx");
+        excelUtil.readExcel_Department("大客户部",resourceAsStream2,map);
+        //获取到部门员工的数据
+        InputStream resourceAsStream3 = excelUtil.getClass().getClassLoader().getResourceAsStream("市场部-薪酬表.xlsx");
+        excelUtil.readExcel_Department("市场部",resourceAsStream3,map);
+        //获取到部门员工的数据
+        InputStream resourceAsStream4 = excelUtil.getClass().getClassLoader().getResourceAsStream("销售部-薪酬表.xlsx");
+        excelUtil.readExcel_Department("销售部",resourceAsStream4,map);
+
+        //读取员工五险一金申报表
+        InputStream resourceAsStream5 = excelUtil.getClass().getClassLoader().getResourceAsStream("员工五险一金申报表.xlsx");
+        excelUtil.readExcel_Department(resourceAsStream5,map);
+
+
+        Set<String> strings = map.keySet();
+        for(String str:strings){
+            Department department = map.get(str);
+            System.out.println(str+" "+department);
+        }
+
+
+        String str = "C:\\Users\\38306\\Desktop\\企业员工月度工资成本支付表.xlsx";
+        File file = new File(str);
+
+        excelUtil.fillExcel_Department(map,file);
+
+//        String path = ExcelUtil.class.getClassLoader().getResource("企业员工月度工资成本支付表.xlsx").getPath();
+//        System.out.println();
+//        File file = new File("企业员工月度工资成本支付表.xlsx");
+//        System.out.println(file.exists());
+
+
+
+
+    }
+
+
+    /**
+     *
+     * @param departmentName
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    public void readExcel_Department(String departmentName, InputStream inputStream,Map<String,Department> map) throws IOException {
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(inputStream);
+        XSSFSheet sheetAt = xssfWorkbook.getSheetAt(0);
+        Iterator<Row> rowIterator = sheetAt.rowIterator();
+        //排除掉表头
+        rowIterator.hasNext();
+        Row head = rowIterator.next();
+        while (rowIterator.hasNext()){
+            Row row = rowIterator.next();
+            Department department = new Department();
+            department.setDepartment(departmentName);
+            department.setJobNumber(row.getCell(0).getStringCellValue());
+            department.setName(row.getCell(1).getStringCellValue());
+            department.setBasicSalary(row.getCell(2).getNumericCellValue());
+            department.setPostWage(row.getCell(3).getNumericCellValue());
+            department.setPerformanceBonus(row.getCell(4).getNumericCellValue());
+            department.setAttendanceBonus(row.getCell(5).getNumericCellValue());
+            department.setAttendanceDeduction(row.getCell(6).getNumericCellValue());
+            department.setPenalties_for_non_compliance(row.getCell(7).getNumericCellValue());
+            department.setTransportationSubsidies(row.getCell(8).getNumericCellValue());
+            department.setCommunicationsGrants(row.getCell(9).getNumericCellValue());
+            map.put(department.getJobNumber(),department);
+        }
+        xssfWorkbook.close();
     }
 
     /**
-     * 填充Excel
-     * @param data 用户数据
-     * @param destFile 目标文件
+     *
+     * @param inputStream
+     * @param map
+     * @throws IOException
      */
-    public void fillExcel(List<Person> data, File destFile) throws IOException {
+    public void readExcel_Department(InputStream inputStream,Map<String,Department> map) throws IOException {
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(inputStream);
+        XSSFSheet sheetAt = xssfWorkbook.getSheetAt(0);
+        Iterator<Row> rowIterator = sheetAt.rowIterator();
+        //排除掉表头
+        rowIterator.hasNext();
+        Row head = rowIterator.next();
+        while (rowIterator.hasNext()){
+            Row row = rowIterator.next();
+            int id = (int)row.getCell(0).getNumericCellValue();
+            double num1 = row.getCell(2).getNumericCellValue(); //工资基数
+            double num2 = row.getCell(3).getNumericCellValue(); //公积金比例
+
+            Department department = map.get(""+id);
+            department.setEndowmentInsuranceSelf(num1*0.08);
+            department.setEndowmentInsuranceCompany(num1*0.2);
+
+            department.setMedicareSelf(num1*0.02);
+            department.setMedicareCompany(num1*0.08);
+
+            department.setUnemploymentInsuranceSelf(num1*0.01);
+            department.setUnemploymentInsuranceConpany(num1*0.02);
+
+            department.setEmploymentInjuryInsuranceSelf(0);
+            department.setEmploymentInjuryInsuranceConpany(num1*0.005);
+
+            department.setBirthInsuranceSelf(num1*0.0);
+            department.setBirthInsuranceConpany(num1*0.007);
+
+            department.setReservedFundsSelf(num1*num2);
+            department.setReservedFundsConpany(num1*num2);
+
+            map.put(""+id,department);
+        }
+        xssfWorkbook.close();
+    }
+
+    /**
+     *
+     * @param data
+     * @param destFile
+     * @throws IOException
+     */
+    public void fillExcel_Department(Map<String,Department> data, File destFile) throws IOException {
         //创建一个工作簿
         XSSFWorkbook xssfWorkbook = new XSSFWorkbook();
         //在工作簿里创建一张表
         XSSFSheet sheet = xssfWorkbook.createSheet("用户");
-        //表中创建一行
-        XSSFRow row = sheet.createRow(0);
         //行中添加数据
-        row.createCell(0).setCellValue("名称");
-        row.createCell(1).setCellValue("城市");
-        row.createCell(2).setCellValue("手机号码");
-        for(int i=0;i<data.size();i++){
-            Person person = data.get(i);
-            System.out.println(person);
-            XSSFRow r = sheet.createRow(i+1);
-            r.createCell(0).setCellValue(person.getName());
-            r.createCell(1).setCellValue(person.getCity());
-            r.createCell(2).setCellValue(person.getCellPhone());
+        Set<String> keys = data.keySet();
+        int i=2;
+        for(String str:keys){
+            Department department = data.get(str);
+            //正收益
+            double add =department.getAdd();
+            //考勤扣除、违规处罚等负收益项
+            double decrease=department.getMinusYield();
+            //应发工资=工资-扣款
+            double wages = department.getSalary();
+            XSSFRow r = sheet.createRow(i++);
+            r.createCell(0).setCellValue(department.getJobNumber());
+            r.createCell(1).setCellValue(department.getName());
+            r.createCell(2).setCellValue(department.getDepartment());
+            r.createCell(3).setCellValue(add);
+            r.createCell(4).setCellValue(decrease);
+            r.createCell(20).setCellValue(wages);
         }
         FileOutputStream fileOutputStream = new FileOutputStream(destFile);
         xssfWorkbook.write(fileOutputStream);
